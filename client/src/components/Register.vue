d9c33293020372ece9c9189c061c956a81463c7d
igorshinal <igorshinal@gmail.com>
2018-01-26 20:12:34 +0200

    sequelize integrate, user model create, validation policy, vuetify material

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,14 +1,34 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type="email" v-model="email" name="email" placeholder="email"><br/>
-    <input type="password" v-model="password" name="password" placeholder="password"><br>
-    <button
-      @click="register">
-      Register
-    </button>
-
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <panel title="Register">
+        <form
+          name="tab-tracker-form"
+          autocomplete="off">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          ></v-text-field>
+          <br>
+          <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+            autocomplete="new-password"
+          ></v-text-field>
+        </form>
+        <br>
+        <div class="danger-alert" v-html="error" />
+        <br>
+        <v-btn
+          dark
+          class="cyan"
+          @click="register">
+          Register
+        </v-btn>
+      </panel>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -18,15 +38,20 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     register: async function () {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
---
+++ server/src/routes.js
@@ -0,0 +1,7 @@
+const AuthenticationController = require('./controller/AuthenticationController')
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+module.exports = (app) => {
+  app.post('/register',
+    AuthenticationControllerPolicy.register,
+    AuthenticationController.register)
+}
